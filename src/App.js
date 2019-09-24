2fd6d0a9d6f6a26d6249a77656652c88d5af267b
Nova <novahasley@gmail.com>
2019-09-24 10:29:20 -0500

    created nav bar function

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Nav from './Nav'
 
 function App() {
   return (
     <div className="App">
-      <nav>
-        
-      </nav>
+      <Nav />
       <h1>Nova Blog</h1>
     </div>
   );
---
+++ src/Nav/Nav.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+
+const Nav = () => {
+    return (
+      <div>
+        <h3>I am a nav bar!</h3>
+      </div>
+    );
+}
+
+export default Nav
